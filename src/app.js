6e16e3123a1cff74a2686a65d5f501a6d86dff77
Szavai Balazs <szavaibalazs@ktch.hu>
2023-03-19 18:46:37 +0100

    Fejrész hozzáadva

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+/*
+* File: app.js
+* Author: Szávai Balázs
+* Copyright: 2023/03/10 Szávai Balázs
+* Group: Szoft I/1/E
+* ÍDate: 2023/03/10
+* Github: https://github.com/szavaibali/
+* Licenc: GNU GPU
+*/
 const NevElem = document.querySelector('nev');
 const KerekElem = document.querySelector('nev');
 const HasznalatElem = document.querySelector('nev');
