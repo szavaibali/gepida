b65a1b29cd3290825ff4ae7bc0156c34700b9da8
Szavai Balazs <szavaibalazs@ktch.hu>
2023-03-19 18:41:37 +0100

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const NevElem = document.querySelector('nev');
+const KerekElem = document.querySelector('nev');
+const HasznalatElem = document.querySelector('nev');
+const ArElem = document.querySelector('nev');
+
